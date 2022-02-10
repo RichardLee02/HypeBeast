0d2d86d8f512f5ab7dfd478a5b403e037d07dd58
Richard Lee <richardzhyulee@gmail.com>
2022-02-09 20:56:36 -0800

    Trying Out Autobot

--- src/test/model/ClothingTest.java
+++ src/test/model/ClothingTest.java
@@ -8,12 +8,22 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ClothingTest {
 
     private Clothing clothingInvalid;
-    private Clothing clothingValid;
+    private Clothing clothingNWT;
+    private Clothing clothingNWOT;
+    private Clothing clothingVEUC;
+    private Clothing clothingEUC;
+    private Clothing clothingVGUC;
+    private Clothing clothingGUC;
 
     @BeforeEach
     public void setUp() {
         clothingInvalid = new Clothing("", "", "", "", 0,0);
-        clothingValid = new Clothing("", "", "NWT", "", 0,0);
+        clothingNWT = new Clothing("", "", "NWT", "", 0,0);
+        clothingNWOT = new Clothing("", "", "NWOT", "", 0,0);
+        clothingVEUC = new Clothing("", "", "VEUC", "", 0,0);
+        clothingEUC = new Clothing("", "", "EUC", "", 0,0);
+        clothingVGUC = new Clothing("", "", "VGUC", "", 0,0);
+        clothingGUC = new Clothing("", "", "GUC", "", 0,0);
     }
 
     @Test
@@ -27,13 +37,63 @@ public class ClothingTest {
     }
 
     @Test
-    public void testConstructorValid() {
-        assertEquals("", clothingValid.getBrand());
-        assertEquals("", clothingValid.getModel());
-        assertEquals("NWT", clothingValid.getCondition());
-        assertEquals("", clothingValid.getSize());
-        assertEquals(0 , clothingValid.getBoughtPrice());
-        assertEquals(0 , clothingValid.getMarketPrice());
+    public void testConstructorNWT() {
+        assertEquals("", clothingNWT.getBrand());
+        assertEquals("", clothingNWT.getModel());
+        assertEquals("NWT", clothingNWT.getCondition());
+        assertEquals("", clothingNWT.getSize());
+        assertEquals(0 , clothingNWT.getBoughtPrice());
+        assertEquals(0 , clothingNWT.getMarketPrice());
+    }
+
+    @Test
+    public void testConstructorNWOT() {
+        assertEquals("", clothingNWOT.getBrand());
+        assertEquals("", clothingNWOT.getModel());
+        assertEquals("NWOT", clothingNWOT.getCondition());
+        assertEquals("", clothingNWOT.getSize());
+        assertEquals(0 , clothingNWOT.getBoughtPrice());
+        assertEquals(0 , clothingNWOT.getMarketPrice());
+    }
+
+    @Test
+    public void testConstructorVEUC() {
+        assertEquals("", clothingVEUC.getBrand());
+        assertEquals("", clothingVEUC.getModel());
+        assertEquals("VEUC", clothingVEUC.getCondition());
+        assertEquals("", clothingVEUC.getSize());
+        assertEquals(0 , clothingVEUC.getBoughtPrice());
+        assertEquals(0 , clothingVEUC.getMarketPrice());
+    }
+
+    @Test
+    public void testConstructorEUC() {
+        assertEquals("", clothingEUC.getBrand());
+        assertEquals("", clothingEUC.getModel());
+        assertEquals("EUC", clothingEUC.getCondition());
+        assertEquals("", clothingEUC.getSize());
+        assertEquals(0 , clothingEUC.getBoughtPrice());
+        assertEquals(0 , clothingEUC.getMarketPrice());
+    }
+
+    @Test
+    public void testConstructorVGUC() {
+        assertEquals("", clothingVGUC.getBrand());
+        assertEquals("", clothingVGUC.getModel());
+        assertEquals("VGUC", clothingVGUC.getCondition());
+        assertEquals("", clothingVGUC.getSize());
+        assertEquals(0 , clothingVGUC.getBoughtPrice());
+        assertEquals(0 , clothingVGUC.getMarketPrice());
+    }
+
+    @Test
+    public void testConstructorGUC() {
+        assertEquals("", clothingGUC.getBrand());
+        assertEquals("", clothingGUC.getModel());
+        assertEquals("GUC", clothingGUC.getCondition());
+        assertEquals("", clothingGUC.getSize());
+        assertEquals(0 , clothingGUC.getBoughtPrice());
+        assertEquals(0 , clothingGUC.getMarketPrice());
     }
 
 }
--- src/test/model/ShoesTest.java
+++ src/test/model/ShoesTest.java
@@ -8,12 +8,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ShoesTest {
 
     private Shoes shoesInvalid;
-    private Shoes shoesValid;
+    private Shoes shoesDS;
+    private Shoes shoesDSWT;
+    private Shoes shoesVNDS;
+    private Shoes shoesNDS;
+    private Shoes shoesPADS;
+    private Shoes shoesBS;
 
     @BeforeEach
     public void setUp() {
         shoesInvalid = new Shoes("", "", "", 0, 0, 0);
-        shoesValid = new Shoes("", "", "DS", 0, 0, 0);
+        shoesDS = new Shoes("", "", "DS", 0, 0, 0);
+        shoesDSWT = new Shoes("", "", "DSWT", 0, 0, 0);
+        shoesVNDS = new Shoes("", "", "VNDS", 0, 0, 0);
+        shoesNDS = new Shoes("", "", "NDS", 0, 0, 0);
+        shoesPADS = new Shoes("", "", "PADS", 0, 0, 0);
+        shoesBS = new Shoes("", "", "BS", 0, 0, 0);
+
     }
 
     @Test
@@ -27,13 +38,63 @@ public class ShoesTest {
     }
 
     @Test
-    public void testConstructorValid() {
-        assertEquals("", shoesValid.getBrand());
-        assertEquals("", shoesValid.getModel());
-        assertEquals("DS", shoesValid.getCondition());
-        assertEquals(0, shoesValid.getSize());
-        assertEquals(0 , shoesValid.getBoughtPrice());
-        assertEquals(0 , shoesValid.getMarketPrice());
+    public void testConstructorDS() {
+        assertEquals("", shoesDS.getBrand());
+        assertEquals("", shoesDS.getModel());
+        assertEquals("DS", shoesDS.getCondition());
+        assertEquals(0, shoesDS.getSize());
+        assertEquals(0 , shoesDS.getBoughtPrice());
+        assertEquals(0 , shoesDS.getMarketPrice());
+    }
+
+    @Test
+    public void testConstructorDSWT() {
+        assertEquals("", shoesDSWT.getBrand());
+        assertEquals("", shoesDSWT.getModel());
+        assertEquals("DSWT", shoesDSWT.getCondition());
+        assertEquals(0, shoesDSWT.getSize());
+        assertEquals(0 , shoesDSWT.getBoughtPrice());
+        assertEquals(0 , shoesDSWT.getMarketPrice());
+    }
+
+    @Test
+    public void testConstructorVNDS() {
+        assertEquals("", shoesVNDS.getBrand());
+        assertEquals("", shoesVNDS.getModel());
+        assertEquals("VNDS", shoesVNDS.getCondition());
+        assertEquals(0, shoesVNDS.getSize());
+        assertEquals(0 , shoesVNDS.getBoughtPrice());
+        assertEquals(0 , shoesVNDS.getMarketPrice());
+    }
+
+    @Test
+    public void testConstructorNDS() {
+        assertEquals("", shoesNDS.getBrand());
+        assertEquals("", shoesNDS.getModel());
+        assertEquals("NDS", shoesNDS.getCondition());
+        assertEquals(0, shoesNDS.getSize());
+        assertEquals(0 , shoesNDS.getBoughtPrice());
+        assertEquals(0 , shoesNDS.getMarketPrice());
+    }
+
+    @Test
+    public void testConstructorPADS() {
+        assertEquals("", shoesPADS.getBrand());
+        assertEquals("", shoesPADS.getModel());
+        assertEquals("PADS", shoesPADS.getCondition());
+        assertEquals(0, shoesPADS.getSize());
+        assertEquals(0 , shoesPADS.getBoughtPrice());
+        assertEquals(0 , shoesPADS.getMarketPrice());
+    }
+
+    @Test
+    public void testConstructorBS() {
+        assertEquals("", shoesBS.getBrand());
+        assertEquals("", shoesBS.getModel());
+        assertEquals("BS", shoesBS.getCondition());
+        assertEquals(0, shoesBS.getSize());
+        assertEquals(0 , shoesBS.getBoughtPrice());
+        assertEquals(0 , shoesBS.getMarketPrice());
     }
 
 }
